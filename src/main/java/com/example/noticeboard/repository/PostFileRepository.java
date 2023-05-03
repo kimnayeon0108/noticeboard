495a7937b3fa6f98e8955a982ba2150a25bfc6a9
kimnayeon <kimnayeon0108@gmail.com>
2023-05-03 14:54:30 +0900

    Param 어노테이션 추가

--- src/main/java/com/example/noticeboard/repository/PostFileRepository.java
+++ src/main/java/com/example/noticeboard/repository/PostFileRepository.java
@@ -4,10 +4,11 @@ import com.example.noticeboard.domain.PostFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostFileRepository extends JpaRepository<PostFile, Long> {
 
     @Modifying
     @Query("UPDATE PostFile pf SET pf.isDeleted = true WHERE pf.post.id = :postId")
-    int deleteAllByPostId(long postId);
+    int deleteAllByPostId(@Param("postId") long postId);
 }
