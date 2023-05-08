109c2382e7a33fa29095698bdaea4d8c07d15462
kimnayeon <kimnayeon0108@gmail.com>
2023-05-08 16:29:54 +0900

    User의 loginId 필드를 email로 변경

--- src/main/java/com/example/noticeboard/domain/User.java
+++ src/main/java/com/example/noticeboard/domain/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String loginId;
+    private String email;
     private String password;
     private String name;
     private boolean status;
