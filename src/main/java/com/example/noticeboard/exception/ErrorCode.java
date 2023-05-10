9119aba5a80a788afb1cbfd120a546fdcd757df7
kimnayeon <kimnayeon0108@gmail.com>
2023-05-10 14:24:34 +0900

    파일 업로드 개수 초과 에러 코드 추가

--- src/main/java/com/example/noticeboard/exception/ErrorCode.java
+++ src/main/java/com/example/noticeboard/exception/ErrorCode.java
@@ -8,9 +8,10 @@ public enum ErrorCode {
 
     // post
     POST_NOT_FOUND("0201", "게시글이 존재하지 않습니다."),
-    INVALID_POST_PASSWORD("0202", "게시글 비밀번호가 일치하지 않습니다."),
-    INACTIVE_COMMENT_POST("0203", "댓글 작성이 불가한 게시글입니다."),
-    POST_NOT_MATCH("0204", "게시글이 일치하지 않습니다."),
+    POST_FILE_EXCEEDED("0202", "파일 업로드는 3개까지 가능합니다."),
+    INVALID_POST_PASSWORD("0203", "게시글 비밀번호가 일치하지 않습니다."),
+    INACTIVE_COMMENT_POST("0204", "댓글 작성이 불가한 게시글입니다."),
+    POST_NOT_MATCH("0205", "게시글이 일치하지 않습니다."),
 
     // comment
     COMMENT_NOT_FOUND("0301", "댓글이 존재하지 않습니다."),
