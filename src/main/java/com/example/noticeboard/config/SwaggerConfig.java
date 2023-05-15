57fc184a257f2b4ffb908bea0bcbe8cfeff7331e
kimnayeon <kimnayeon0108@gmail.com>
2023-05-15 15:02:37 +0900

    swagger에서 Authorization 헤더에 Bearer 토큰 추가

--- src/main/java/com/example/noticeboard/config/SwaggerConfig.java
+++ src/main/java/com/example/noticeboard/config/SwaggerConfig.java
@@ -2,10 +2,28 @@ package com.example.noticeboard.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(info = @Info(title = "게시판 api 명세서", description = "api 명세서", version = "v1"))
 @Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components().addSecuritySchemes(jwtSchemeName,
+                new SecurityScheme().name(jwtSchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("Bearer"));
+
+        return new OpenAPI().addSecurityItem(securityRequirement)
+                            .components(components);
+    }
 }
