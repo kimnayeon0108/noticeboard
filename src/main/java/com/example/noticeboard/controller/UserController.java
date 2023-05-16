c5a61269d53a6c98e3d77db3c4c43f65df4ac941
kimnayeon <kimnayeon0108@gmail.com>
2023-05-16 11:54:01 +0900

    swagger 문서 설명 추가

--- src/main/java/com/example/noticeboard/controller/UserController.java
+++ src/main/java/com/example/noticeboard/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.noticeboard.dto.response.ResponseDto;
 import com.example.noticeboard.security.dto.UserDetailsDto;
 import com.example.noticeboard.service.UserService;
 import com.example.noticeboard.type.UserRole;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -24,18 +25,21 @@ public class UserController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "회원가입", description = "일반 회원용 가입 api", tags = "user")
     public ResponseDto<ResUserDto> signup(@Valid @RequestBody ReqSignupDto reqSignupDto) {
         return ResponseDto.success(userService.signup(reqSignupDto, UserRole.ROLE_USER));
     }
 
     @PostMapping("/admin/signup")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "관리자 회원가입", description = "관리자용 가입 api", tags = "user")
     public ResponseDto<ResUserDto> adminSignup(@Valid @RequestBody ReqSignupDto reqSignupDto) {
         return ResponseDto.success(userService.signup(reqSignupDto, UserRole.ROLE_ADMIN));
     }
 
     @GetMapping("/user/profile")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "본인 정보 조회", description = "유저 정보 조회 api", tags = "user")
     public ResponseDto<ResUserDto> showProfile(@Parameter(hidden = true) @AuthenticationPrincipal UserDetailsDto userDetails) {
         return ResponseDto.success(userService.getProfile(userDetails.getUsername()));
     }
