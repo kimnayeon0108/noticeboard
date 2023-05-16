09b7153a30b1688b335c2650c6ac85258b2fb45a
kimnayeon <kimnayeon0108@gmail.com>
2023-05-16 11:54:13 +0900

    validation 수정

--- src/main/java/com/example/noticeboard/dto/request/ReqValidatePostPasswordDto.java
+++ src/main/java/com/example/noticeboard/dto/request/ReqValidatePostPasswordDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -13,8 +13,8 @@ import javax.validation.constraints.Pattern;
 @Schema(description = "게시글 비밀번호 검증 DTO")
 public class ReqValidatePostPasswordDto {
 
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "영문, 숫자 포함 길이가 4이상 8이하이어야 합니다.")
     @Length(min = 4, max = 8)
     @Schema(description = "게시글 비밀번호", required = true, minLength = 4, maxLength = 8)
     private String password;
